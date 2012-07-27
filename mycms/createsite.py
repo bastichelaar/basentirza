e2e0642b1da2523af5575bf2d2f994b6b3dbe40f
Bas Tichelaar <bas@30loops.net>
2012-07-27 20:40:07 +0200

    Switched to django 1.4

--- mycms/createsite.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.sites.models import Site
-Site.objects.create(
-        pk=1,
-        domain='30loops-app-djangocms-production.30loops.net',
-        name='30loops-app-djangocms-production.30loops.net')
