2157c0e2e7dfb00e0decf5c28cf88e009be8abb5
Bas Tichelaar <bas@30loops.net>
2012-07-27 20:44:41 +0200

    Switched to django 1.4

---
+++ basentirza/createsite.py
@@ -0,0 +1,5 @@
+from django.contrib.sites.models import Site
+Site.objects.create(
+        pk=1,
+        domain='bastichelaar-app-basentirza2.30loops.net',
+        name='bastichelaar-app-basentirza2.30loops.net')
