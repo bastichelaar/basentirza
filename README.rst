d70c2176361b4caaed40d4dfb2e84afa3153c6d8
Bas Tichelaar <bas@30loops.net>
2012-07-27 20:25:13 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+This Django project is used as an example for the user documentation for the
+`30loops.net`_ platform. Visit http://30loops.net/docs for details.
+
+.. _`30loops.net`: http://30loops.net
---
+++ mycms/createsite.py
@@ -0,0 +1,5 @@
+from django.contrib.sites.models import Site
+Site.objects.create(
+        pk=1,
+        domain='30loops-app-djangocms-production.30loops.net',
+        name='30loops-app-djangocms-production.30loops.net')
